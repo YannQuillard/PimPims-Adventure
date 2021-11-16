112de2d991ab477837adc3f21f7bbbbeee5198ae
Alexis Kessab <alexis.kessab@orange.fr>
2021-11-16 13:51:35 +0100

    Update MoveBDN.cs

--- Scripts/MoveBDN.cs
+++ Scripts/MoveBDN.cs
@@ -5,18 +5,18 @@ using UnityEngine;
 public class MoveBDN : MonoBehaviour
 
 {
-    public Vector3 Deplacement = new Vector3 (0, 1, 0) ;
-    public float Vitesse; 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public Vector3 Deplacement = new Vector3 (0, 1, 0) ;
+	public float Vitesse; 
+	// Start is called before the first frame update
+	void Start()
+	{
+		
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        transform.Translate(Deplacement.normalized * Vitesse * Time.deltaTime);
-        
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		transform.Translate(Deplacement.normalized * Vitesse * Time.deltaTime);
+		
+	}
 }
